e7bddd22d88888b512245fb4f1e5500de12d12d7
Carlos Santoro <csantoro@emeals.com>
2024-03-05 19:25:22 -0800

    Add aws utils package.

---
+++ utils/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='utils',
+    version='0.0.1',
+    packages=find_packages(),
+    license='MIT',
+    author='santoroc',
+    description='ETL utilities to use with aws.',
+    setup_requires=['wheel']
+)
